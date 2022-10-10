981fa4d4a868024f9589ea0b66437264f72a8d14
Cahyo <cahyo.adi.permono@gmail.com>
2022-10-10 08:01:19 +0700

    fixing and adding translation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,52 +1,49 @@
 const express = require('express');
 const UserService = require('./UserService');
-const { check, validationResult } = require('express-validator');
-
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 router.post(
   '/api/1.0/users',
   check('username')
     .notEmpty()
-    .withMessage('Username cannot be null')
+    .withMessage('username_null')
     .bail()
     .isLength({ min: 4, max: 32 })
-    .withMessage('Must Have min 4 and max 32 char'),
+    .withMessage('username_size'),
   check('email')
     .notEmpty()
-    .withMessage('Email cannot be null')
+    .withMessage('email_null')
     .bail()
     .isEmail()
-    .withMessage('Email must be valid')
+    .withMessage('email_invalid')
     .bail()
     .custom(async (email) => {
-      const isEmailExists = await UserService.isEmailExists(email);
-      if (isEmailExists) {
-        throw new Error('Email in use');
+      const user = await UserService.findByEmail(email);
+      if (user) {
+        throw new Error('email_inuse');
       }
     }),
   check('password')
     .notEmpty()
-    .withMessage('Password cannot be null')
+    .withMessage('password_null')
     .bail()
-    .isLength({ min: 8 })
-    .withMessage('Password must have at least 8 char')
+    .isLength({ min: 6 })
+    .withMessage('password_size')
     .bail()
-    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)
-    .withMessage(
-      'Password must have at least 1 lowercase and 1 uppercase and 1 number'
-    ),
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage('password_pattern'),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const validationErrors = {};
       errors
         .array()
-        .forEach((error) => (validationErrors[error.param] = error.msg));
-      return res.status(400).send({ validationErrors });
+        .forEach((error) => (validationErrors[error.param] = req.t(error.msg)));
+      return res.status(400).send({ validationErrors: validationErrors });
     }
     await UserService.save(req.body);
-    return res.send({ message: 'User Created' });
+    return res.send({ message: req.t('user_create_success') });
   }
 );
 
