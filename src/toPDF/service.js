20f8fcaf08773f0f90bea329c900e8b8e782c3f3
zettravos321 <sidna.zen@imaniprima.com>
2022-11-17 11:37:45 +0700

    :sparkles: new feature: generate pdf and send email
    new feature: generate pdf and send email

--- src/toPDF/service.js
+++ src/toPDF/service.js
@@ -8,19 +8,19 @@ const ApplicantService = require("../applicant/ApplicantService");
 const SubSkillService = require('../skill/SubSkillService');
 
 const options = {
-  format: "A4",
-  orientation: "potrait",
-  border: "10mm",
+    format: "A4",
+    orientation: "potrait",
+    border: "10mm",
 };
 router.get("/api/1.0/download_pdf/:id", async (req, res) => {
-  const filename = "applicantDocs_" + req.params.id + ".pdf";
-  fs.readFile("./docs/" + filename, "base64", (err, dataPDF) => {
-    if (err) {
-      res.send({ message: "error !" });
-    } else {
-      res.send({ dataPDF });
-    }
-  });
+    const filename = "applicantDocs_" + req.params.id + ".pdf";
+    fs.readFile("./docs/" + filename, "base64", (err, dataPDF) => {
+        if (err) {
+            res.send({ message: "error !" });
+        } else {
+            res.send({ dataPDF });
+        }
+    });
 });
 
 router.get("/api/1.0/topdf_skill/:id", async (req, res) => {
@@ -34,18 +34,14 @@ router.get("/api/1.0/topdf_skill/:id", async (req, res) => {
     dataApplicant.applicantskills.forEach(element => {
         applicantskills.push({ subskill_id: element.subskillId, nilai: element.nilai, keterangan: element.keterangan })
     });
-  });
-  let a = [];
-  const subskill = await SubSkillService.find();
-  subskill.forEach((element) => {
-    applicantskills.forEach((e) => {
-      if (element.id === e.subskill_id) {
-        a.push({
-          subskill: element.subskill,
-          nilai: e.nilai,
-          keterangan: e.keterangan,
+    let a = []
+    const subskill = await SubSkillService.find();
+    subskill.forEach(element => {
+        applicantskills.forEach(e => {
+            if (element.id === e.subskill_id) {
+                a.push({ skill: element.skill.skill, subskill: element.subskill, nilai: e.nilai, keterangan: e.keterangan })
+            }
         });
-      }
     });
     const skills = [];
     for (let item of a) {
@@ -65,7 +61,6 @@ router.get("/api/1.0/topdf_skill/:id", async (req, res) => {
         })
         c.push(y)
     })
-
     const html = fs.readFileSync(path.join(__dirname, '../toPDF/template/temp_tech.html'), 'utf-8');
     const filename_TechnicalSkill = 'TechApplicantDocs_' + req.params.id + '.pdf';
 
@@ -111,157 +106,156 @@ router.get("/api/1.0/topdf_skill/:id", async (req, res) => {
         } else {
             console.log("email send")
             // Send base64 pdf to client
-            res.send({message:"Email Send!"})
+            res.send({ message: "Email Send!" })
         };
     });
+});
 
-})
 
 router.get("/api/1.0/topdf/:id", async (req, res) => {
-  const html = fs.readFileSync(
-    path.join(__dirname, "../toPDF/template/temp.html"),
-    "utf-8"
-  );
-  const dataApplicant = await ApplicantService.byId(req.params.id);
+    const html = fs.readFileSync(
+        path.join(__dirname, "../toPDF/template/temp.html"),
+        "utf-8"
+    );
+    const dataApplicant = await ApplicantService.byId(req.params.id);
 
-  const filename = "applicantDocs_" + dataApplicant.id + ".pdf";
+    const filename = 'applicantDocs_' + dataApplicant.id + '.pdf';
 
-  const dataApplicantPromise = {
-    name: dataApplicant.name,
-    gender: dataApplicant.gender,
-    place_of_birth: dataApplicant.place_of_birth,
-    date: dataApplicant.date
-      .toISOString()
-      .substring(0, 10)
-      .split("-")
-      .reverse()
-      .join("-"),
-    blood_type: dataApplicant.blood_type,
-    address: dataApplicant.address,
-    postal_code_address: dataApplicant.postal_code_address,
-    domicile: dataApplicant.domicile,
-    postal_code_domicile: dataApplicant.postal_code_domicile,
-    phone: dataApplicant.phone,
-    mobile: dataApplicant.mobile,
-    office_parent_phone: dataApplicant.office_parent_phone,
-    email: dataApplicant.email,
-    id_sim_no: dataApplicant.id_sim_no,
-    valid_to: dataApplicant.valid_to,
-    npwp_no: dataApplicant.npwp_no,
-    account_no: dataApplicant.account_no,
-    religion: dataApplicant.religion,
-    position: dataApplicant.position,
-    photo: dataApplicant.photo,
-    marital_status: dataApplicant.marital_status,
-    year_marriage: dataApplicant.year_marriage,
-  };
-
-  const families = dataApplicant.families;
-  const familiesPromise = [];
-  families.forEach((element) => {
-    let args = {
-      member: element.member,
-      name: element.name,
-      gender: element.gender,
-      date: element.date
-        .toISOString()
-        .substring(0, 10)
-        .split("-")
-        .reverse()
-        .join("-"),
-      education: element.education,
-      occupation_company: element.occupation_company,
+    const dataApplicantPromise = {
+        name: dataApplicant.name,
+        gender: dataApplicant.gender,
+        place_of_birth: dataApplicant.place_of_birth,
+        date: dataApplicant.date.toISOString().substring(0, 10).split("-").reverse().join("-"),
+        blood_type: dataApplicant.blood_type,
+        address: dataApplicant.address,
+        postal_code_address: dataApplicant.postal_code_address,
+        domicile: dataApplicant.domicile,
+        postal_code_domicile: dataApplicant.postal_code_domicile,
+        phone: dataApplicant.phone,
+        mobile: dataApplicant.mobile,
+        office_parent_phone: dataApplicant.office_parent_phone,
+        email: dataApplicant.email,
+        id_sim_no: dataApplicant.id_sim_no,
+        valid_to: dataApplicant.valid_to.toISOString().substring(0, 10).split("-").reverse().join("-"),
+        npwp_no: dataApplicant.npwp_no,
+        account_no: dataApplicant.account_no,
+        religion: dataApplicant.religion,
+        position: dataApplicant.position,
+        photo: dataApplicant.photo,
+        marital_status: dataApplicant.marital_status,
+        year_marriage: dataApplicant.year_marriage,
     };
-    familiesPromise.push(args);
-  });
 
-  const formaleducation = dataApplicant.formaleducations;
-  const formaleducationPromise = [];
-  formaleducation.forEach((element) => {
-    let args = {
-      level: element.level,
-      name_location: element.name_location,
-      major: element.major,
-      entry: element.entry,
-      graduate: element.graduate,
+    const families = dataApplicant.families;
+    const familiesPromise = [];
+    families.forEach((element) => {
+        let args = {
+            member: element.member,
+            name: element.name,
+            gender: element.gender,
+            date: element.date
+                .toISOString()
+                .substring(0, 10)
+                .split("-")
+                .reverse()
+                .join("-"),
+            education: element.education,
+            occupation_company: element.occupation_company,
+        };
+        familiesPromise.push(args);
+    });
+
+    const formaleducation = dataApplicant.formaleducations;
+    const formaleducationPromise = [];
+    formaleducation.forEach((element) => {
+        let args = {
+            level: element.level,
+            name_location: element.name_location,
+            major: element.major,
+            entry: element.entry,
+            graduate: element.graduate,
+        };
+        formaleducationPromise.push(args);
+    });
+
+    const nonformaleducation = dataApplicant.nonformaleducations;
+    const nonformaleducationsPromise = [];
+    nonformaleducation.forEach((element) => {
+        let args = {
+            course: element.course,
+            year: element.year,
+            duration: element.duration,
+            certificate: element.certificate,
+            sponsored_by: element.sponsored_by,
+        };
+        nonformaleducationsPromise.push(args);
+    });
+
+    const computerliterates = dataApplicant.computerliterates;
+    const computerliteratesPromise = [];
+    computerliterates.forEach((element) => {
+        let args = {
+            skill: element.skill,
+            level: element.level,
+        };
+        computerliteratesPromise.push(args);
+    });
+
+    const employmenthistories = dataApplicant.employmenthistories;
+    const employmenthistoriesPromise = [];
+    employmenthistories.forEach((element) => {
+        let args = {
+            start: element.start,
+            end: element.end,
+            name_company: element.name_company,
+            position: element.position,
+            direct_supervisor: element.direct_supervisor,
+            take_home_pay: element.take_home_pay,
+            reason_leaving: element.reason_leaving,
+        };
+        employmenthistoriesPromise.push(args);
+    });
+
+    const jobdescriptionPromise = {
+        desciption: dataApplicant.jobdescription.description,
     };
-    formaleducationPromise.push(args);
-  });
 
-  const nonformaleducation = dataApplicant.nonformaleducations;
-  const nonformaleducationsPromise = [];
-  nonformaleducation.forEach((element) => {
-    let args = {
-      course: element.course,
-      year: element.year,
-      duration: element.duration,
-      certificate: element.certificate,
-      sponsored_by: element.sponsored_by,
+    const otherinformationPromise = {
+        hospitalized: dataApplicant.otherinformation.hospitalized,
+        disease: dataApplicant.otherinformation.disease,
+        psycological_test: dataApplicant.otherinformation.psycological_test,
+        experience_tellecomunication:
+            dataApplicant.otherinformation.experience_tellecomunication,
+        experience_it: dataApplicant.otherinformation.experience_it,
+        reason_join: dataApplicant.otherinformation.reason_join,
+        reason_hire: dataApplicant.otherinformation.reason_hire,
+        opinion_teamwork: dataApplicant.otherinformation.opinion_teamwork,
+        plan: dataApplicant.otherinformation.plan,
+        respond_target: dataApplicant.otherinformation.respond_target,
+        respond_preasure: dataApplicant.otherinformation.respond_preasure,
+        reason_leave_last_company:
+            dataApplicant.otherinformation.reason_leave_last_company,
+        salary_expect: dataApplicant.otherinformation.salary_expect,
+        able_to_start: dataApplicant.otherinformation.able_to_start,
+        contact_emergency: dataApplicant.otherinformation.contact_emergency,
+        relatives_in_ip: dataApplicant.otherinformation.relatives_in_ip,
+        strength: dataApplicant.otherinformation.strength,
+        weakness: dataApplicant.otherinformation.weakness,
     };
-    nonformaleducationsPromise.push(args);
-  });
 
-  const computerliterates = dataApplicant.computerliterates;
-  const computerliteratesPromise = [];
-  computerliterates.forEach((element) => {
-    let args = {
-      skill: element.skill,
-      level: element.level,
-    };
-    computerliteratesPromise.push(args);
-  });
+    const attachmentapplicants = dataApplicant.attachmentapplicants;
+    const attachmentapplicantsPromise = [];
+    attachmentapplicants.forEach((element) => {
+        let args = {
+            type: element.type,
+            file: element.file,
+        };
+        attachmentapplicantsPromise.push(args);
+    });
 
-  const employmenthistories = dataApplicant.employmenthistories;
-  const employmenthistoriesPromise = [];
-  employmenthistories.forEach((element) => {
-    let args = {
-      start: element.start,
-      end: element.end,
-      name_company: element.name_company,
-      position: element.position,
-      direct_supervisor: element.direct_supervisor,
-      take_home_pay: element.take_home_pay,
-      reason_leaving: element.reason_leaving,
-    };
-    employmenthistoriesPromise.push(args);
-  });
-
-  const jobdescriptionPromise = {
-    desciption: dataApplicant.jobdescription.description,
-  };
-
-  const otherinformationPromise = {
-    hospitalized: dataApplicant.otherinformation.hospitalized,
-    disease: dataApplicant.otherinformation.disease,
-    psycological_test: dataApplicant.otherinformation.psycological_test,
-    experience_tellecomunication:
-      dataApplicant.otherinformation.experience_tellecomunication,
-    experience_it: dataApplicant.otherinformation.experience_it,
-    reason_join: dataApplicant.otherinformation.reason_join,
-    reason_hire: dataApplicant.otherinformation.reason_hire,
-    opinion_teamwork: dataApplicant.otherinformation.opinion_teamwork,
-    plan: dataApplicant.otherinformation.plan,
-    respond_target: dataApplicant.otherinformation.respond_target,
-    respond_preasure: dataApplicant.otherinformation.respond_preasure,
-    reason_leave_last_company:
-      dataApplicant.otherinformation.reason_leave_last_company,
-    salary_expect: dataApplicant.otherinformation.salary_expect,
-    able_to_start: dataApplicant.otherinformation.able_to_start,
-    contact_emergency: dataApplicant.otherinformation.contact_emergency,
-    relatives_in_ip: dataApplicant.otherinformation.relatives_in_ip,
-    strength: dataApplicant.otherinformation.strength,
-    weakness: dataApplicant.otherinformation.weakness,
-  };
-
-  const attachmentapplicants = dataApplicant.attachmentapplicants;
-  const attachmentapplicantsPromise = [];
-  attachmentapplicants.forEach((element) => {
-    let args = {
-      type: element.type,
-      file: element.file,
-    };
-    attachmentapplicantsPromise.push(args);
-  });
+    //dataApplicantPromise, familiesPromise, formaleducationPromise, nonformaleducationsPromise, 
+    //computerliteratesPromise, employmenthistoriesPromise, jobdescriptionPromise, otherinformationPromise,
+    //attachmentapplicantsPromise
 
     const document = {
         html: html,
@@ -278,13 +272,14 @@ router.get("/api/1.0/topdf/:id", async (req, res) => {
         },
         path: './docs/' + filename
     }
+    const filepath = 'http://localhost:3000/docs/' + filename;
 
     await pdf.create(document, options)
-        .then(res => {
-            console.log(res);
+        .then(() => {
             fs.readFile('./docs/' + filename, 'base64', ((err, dataPDF) => {
                 if (err) throw err;
                 res.send({
+                    filepath: filepath,
                     message: "pdf generated",
                     pdf: dataPDF,
                 });
