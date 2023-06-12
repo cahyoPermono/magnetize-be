8de36b2733ae99927c128c9f489d9c132e85c0c0
zenimaniprima <sidna.zen@imaniprima.co.id>
2023-06-12 09:10:05 +0700

    update email receiver

---
+++ src/config/emailReceiver.js
@@ -0,0 +1,8 @@
+exports.receiver = "sidna.zen@imaniprima.com"
+exports.sender = {
+    service: "hotmail",
+    auth: {
+        user: "auto_notifier_ip@outlook.com",
+        pass: "magnetize2022",
+    },
+}
